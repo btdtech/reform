593747a3029be1bbd59370aa6cea0262205a39f0
Bac Trach Dam <btdtech2021@gmail.com>
2022-04-24 18:19:36 +1000

    Added more form controls.

--- app/src/routes/BasicUsageDemo.tsx
+++ app/src/routes/BasicUsageDemo.tsx
@@ -1,7 +1,15 @@
 import { Container, Grid, Stack, Typography } from '@mui/material';
-import { Form, useForm, FormValues, Field, FieldLabel } from '@btdtech/reform';
+import {
+	Form,
+	useForm,
+	FormValues,
+	Field,
+	FieldLabel,
+	FieldGroup,
+} from '@btdtech/reform';
 import { useState } from 'react';
 import ReactJson from 'react-json-pretty';
+import { Checkable } from '../components/Checkable';
 
 export const BasicUsageDemo = () => {
 	const [form] = useForm();
@@ -32,6 +40,34 @@ export const BasicUsageDemo = () => {
 							<Field name={['legals', 'agreed']} orientation="row">
 								<input type="checkbox" />
 								<FieldLabel>Terms and conditions</FieldLabel>
+							</Field>
+							<FieldGroup name={['skills', 'languages']} multiple>
+								<FieldLabel>Languages</FieldLabel>
+								<Checkable
+									value="javascript"
+									label="Javascript"
+									type="checkbox"
+								/>
+								<Checkable value="java" label="Java" type="checkbox" />
+								<Checkable value="python" label="Python" type="checkbox" />
+							</FieldGroup>
+							<FieldGroup name={['other', 'demographic', 'atsi']}>
+								<FieldLabel>ATSI</FieldLabel>
+								<Checkable value="aboriginal" label="Aboriginal" type="radio" />
+								<Checkable
+									label="Torres Strait Islander"
+									value="torres-strait-islander"
+									type="radio"
+								/>
+								<Checkable value="none" label="None" type="radio" />
+							</FieldGroup>
+							<Field name={['other', 'demographic', 'gender']}>
+								<FieldLabel>Gender</FieldLabel>
+								<select>
+									<option value="male">Male</option>
+									<option value="female">Female</option>
+									<option value="other">Other</option>
+								</select>
 							</Field>
 							<Stack direction={'row-reverse'}>
 								<button type="submit">Submit</button>
